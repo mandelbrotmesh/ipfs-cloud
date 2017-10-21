bf82aa4f1e5a775e5765e15bb778e85842720112
flowpoint <flowpoint@protonmail.com>
2017-10-21 20:14:05 +0200

    playaround with svg

--- src/elm/Layout.elm
+++ src/elm/Layout.elm
@@ -2,34 +2,59 @@ module Layout exposing (..)
 
 import Html exposing (Html)
 import Color exposing (..)
-import Style exposing (..)
-import Style.Color as Color
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
-import Element exposing (..)
-import Element.Attributes exposing (..)
-import Element.Input as Input
-import Element.Input exposing (search, text, disabled)
 
-type Test =
-  String
+appshell : Html.Html msg
+appshell =
+  svg
+    [ width "100vw"
+    , height "200"]
+    [ g
+        [ width "480"
+        , height "80"]
+        [ rect
+            [ x "0"
+            , y "0"
+            , fill "rgba(255,25,25,1)"
+            , width "480"
+            , height "80"
+            ]
+            []
+        , image --menubutton
+            [ xlinkHref "https://ipfs.io/ipfs/QmcGneXUwhLv49P23kZPQ5LCEi15nQis4PZDrd1jZf75cc/navigation/svg/production/ic_menu_48px.svg"
+            , width "80"
+            ]
+            []
 
--- type Styles
---   = None
---   | Menustyle
---   | Drawerstyle
---   | Mainviewstyle
---
--- type Msg
---   = Acc_submit_msg String
---   | Use_drawer Bool
---
--- stylesheet =
---   Style.styleSheet
---     [ Style.style None []
---     , Style.style Menustyle
---         [ Color.background blue ]
---         [ Color.background blue ]
---     , Style.style Drawerstyle
---     , Style.style Mainviewstyle
---         [ Color.background yellow ]
---     ]
+        , g
+            []
+            [ rect
+                [ fill "rgba(182,182,182,1)"
+                , x "100"
+                , y "10"
+                , width "280"
+                , height "60"
+                , rx "12"
+                , ry "12"
+                ] []
+
+            ]
+        , image --menubutton
+            [ xlinkHref "https://ipfs.io/ipfs/QmcGneXUwhLv49P23kZPQ5LCEi15nQis4PZDrd1jZf75cc/action/svg/production/ic_search_48px.svg"
+            , x "320"
+            , y "10"
+            , width "60"
+
+            ]
+            []
+
+        , image --menubutton
+            [ xlinkHref "https://ipfs.io/ipfs/QmcGneXUwhLv49P23kZPQ5LCEi15nQis4PZDrd1jZf75cc/action/svg/production/ic_account_circle_48px.svg"
+            , x "400"
+            , width "80"
+            ]
+            []
+        ]
+    ]
